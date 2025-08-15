265cf0d18865a04007d1fb0f4bd0369a6425c0c0
lemisora <leonardo.michel.dom@gmail.com>
2025-08-15 15:44:50 -0600

    Se crea el módulo de statistics

---
+++ ng-student-dashboard/src/app/modules/statistics/components/statistics-view/statistics-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-statistics-view',
+  templateUrl: './statistics-view.component.html',
+  styleUrls: ['./statistics-view.component.scss']
+})
+export class StatisticsViewComponent {
+
+}
---
+++ ng-student-dashboard/src/app/modules/statistics/statistics.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StatisticsViewComponent } from './components/statistics-view/statistics-view.component';
+
+
+
+@NgModule({
+  declarations: [
+    StatisticsViewComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class StatisticsModule { }
