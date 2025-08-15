86dc8e06abc5f86248f911bc7b0ed489843a1b59
lemisora <leonardo.michel.dom@gmail.com>
2025-08-15 15:28:08 -0600

    Componentes creados y añadidos a app/components

--- ng-student-dashboard/src/app/app.module.ts
+++ ng-student-dashboard/src/app/app.module.ts
@@ -1,18 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { StudentListComponent } from "./components/student-list/student-list.component";
+import { StudentDetailComponent } from "./components/student-detail/student-detail.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    StudentListComponent,
+    StudentDetailComponent,
+    DashboardComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
